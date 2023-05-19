0b0d89feb78c5d4b9d82d22dbf551e2e3d06fcde
henry <henry.riehl@btinternet.com>
2023-05-19 19:37:00 +0100

    fix build errors

--- lib/views/terminal_frame.dart
+++ lib/views/terminal_frame.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_pty/flutter_pty.dart';
 import 'package:terminal/constants/constants.dart';
 import 'package:xterm/xterm.dart';
